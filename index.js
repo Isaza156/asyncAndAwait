d06733d04938ddc83b08c7edf7c6d252ad3584c4
Isaza156 <jonathan.isaza@hotmail.com>
2019-09-14 12:00:59 -0500

    act

---
+++ index.js
@@ -0,0 +1,61 @@
+let numero1 = 100;
+let numero2 = 1;
+
+// function suma (x, y) {
+//     return x + y
+// }
+// resultado1 = suma(numero1, numero2);
+
+
+// function primo(resultado1){
+//     if( resultado1 % 2 !== 0){
+//         console.log('el numero ' + resultado1 + ' es primo'); 
+//     } else {
+//          console.log('el numero ' + resultado1 + ' no es primo'); 
+//     }      
+// }
+// primo(resultado1);
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+// setTimeout(() => {
+//    let suma = numero1 + numero2;
+//     console.log('La suma es ' + suma)
+// }, 4000);
+
+
+// setTimeout(() => {
+//     let suma = numero1 + numero2;
+//     if( suma % 2 !== 0){
+//         console.log('el numero ' + suma + ' es primo'); 
+//     } else {
+//          console.log('el numero ' + suma + ' no es primo'); 
+//     }      
+// }, 2000);
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+// async function sumaYprimo() {
+//     await suma();
+//     await primo();
+// }
+// sumaYprimo();
+
+
+// function suma() {
+//     return resultado1 = numero1 + numero2;
+// }
+
+
+// function primo() {
+//     let resultado1 = numero1 + numero2;
+//     if (resultado1 % 2 !== 0) {
+//         console.log('el numero ' + resultado1 + ' es primo');
+//     } else {
+//         console.log('el numero ' + resultado1 + ' no es primo');
+//     }
+// }
